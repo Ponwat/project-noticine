0d5da06124e8588342653de26a75c5bbe9f88a42
Ponwat <buddykung9000@gmail.com>
2025-02-28 21:51:53 +0700

    make base.html

--- mobile_doctor/scripts/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('push', function(event) {
-    const options = {
-        body: 'This is a push notification!',
-        vibrate: [200, 100, 200],
-    };
-    event.waitUntil(
-        self.registration.showNotification('Push Notification', options)
-    );
-});
