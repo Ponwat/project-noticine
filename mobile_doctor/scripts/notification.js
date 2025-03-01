3e7424284f34e405cec859bd54af9246ddf9f7f4
Ponwat <buddykung9000@gmail.com>
2025-03-01 23:44:44 +0700

    test notification 5

--- mobile_doctor/scripts/notification.js
+++ mobile_doctor/scripts/notification.js
@@ -1,41 +1,30 @@
 console.log("notification.js loaded");
 
-if ('Notification' in window && 'serviceWorker' in navigator) {
-    alert("Push notifications are supported!");
-} else {
-    alert("Push notifications are NOT supported.");
-}
-
-navigator.serviceWorker.register('./scripts/sw.js')
-    .then(reg => console.log('Service Worker Registered!', reg))
-    .catch(err => console.log('Service Worker Registration Failed!', err));
-
-
-const requsetButton = document.getElementById("request-permission-button");
-requsetButton.onclick = () => {
-    if(typeof(Notification) == "undefined"){
-        console.error("Notification not supported");
-    }
-    var promise = Notification.requestPermission();
-    promise.then((permission) => {
-        if(permission !="granted"){
-            alert("Permission not granted");
-            return;
-        }
-        alert("Permission granted");
-    })
-}
+// const requsetButton = document.getElementById("request-permission-button");
+// requsetButton.onclick = () => {
+//     if(typeof(Notification) == "undefined"){
+//         console.error("Notification not supported");
+//     }
+//     var promise = Notification.requestPermission();
+//     promise.then((permission) => {
+//         if(permission !="granted"){
+//             alert("Permission not granted");
+//             return;
+//         }
+//         alert("Permission granted");
+//     })
+// }
 
 
 const label = document.getElementById("label");
+
+const time_input = document.getElementById("input");
+
 const button = document.getElementById("notification-button");
 button.onclick = function() {
-    alert("Button clicked");
-    if (Android != undefined) {
+    console.log(time_input)
+    if (Android) {
         alert(Android);
         Android.hello("Hello");
     }
-    // const notification = new Notification("Hello, World!");
-    // // alert(notification);
-    // label.innerText = notification; 
 }
