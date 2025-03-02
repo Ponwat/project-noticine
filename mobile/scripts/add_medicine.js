074012a956624f7cd8fa6f97f31f100c0315d6ab
COny_Knight <97920186+COny7897@users.noreply.github.com>
2025-03-02 14:07:03 +0700

    update add medicine.

--- mobile/scripts/add_medicine.js
+++ mobile/scripts/add_medicine.js
@@ -1,14 +1,46 @@
 //----------------------------------IMAGE SECTION ---------------------------------------------
 document.getElementById('imageInput').addEventListener('change', function(event) {
     const file = event.target.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            document.getElementById('medImage').src = e.target.result;
-        }
-        reader.readAsDataURL(file);
-    }
+    
+    document.getElementById('medImage').src = URL.createObjectURL(file);
+    // if (file) {
+    //     const reader = new FileReader();
+    //     reader.onload = function(e) {
+    //         newSrc = downscaleImage(e.target.result, 200, "image/jpeg");
+    //         console.log(newSrc)
+    //         document.getElementById('medImage').src = newSrc;
+    //     }
+    //     reader.readAsDataURL(file);
+    // }
 });
+
+// Take an image URL, downscale it to the given width, and return a new image URL.
+function downscaleImage(dataUrl, newWidth, imageType, imageArguments) {
+    "use strict";
+    var image, oldWidth, oldHeight, newHeight, canvas, ctx, newDataUrl;
+
+    // Provide default values
+    imageType = imageType || "image/jpeg";
+    imageArguments = imageArguments || 0.7;
+
+    // Create a temporary image so that we can compute the height of the downscaled image.
+    image = new Image();
+    image.src = dataUrl;
+    oldWidth = image.width;
+    oldHeight = image.height;
+    newHeight = Math.floor(oldHeight / oldWidth * newWidth)
+
+    // Create a temporary canvas to draw the downscaled image on.
+    canvas = document.createElement("canvas");
+    canvas.width = newWidth;
+    canvas.height = newHeight;
+
+    // Draw the downscaled image on the canvas and return the new data URL.
+    ctx = canvas.getContext("2d");
+    ctx.drawImage(image, 0, 0, newWidth, newHeight);
+    newDataUrl = canvas.toDataURL(imageType, imageArguments);
+    return newDataUrl;
+}
 
 //---------------------------------- UNIT SECTION ---------------------------------------------
 
